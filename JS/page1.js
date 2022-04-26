d46b5165028d88f375d22685795b20f077d68fa7
juansued1233536 <92616927+juansued1233536@users.noreply.github.com>
2022-04-25 23:08:50 -0300

    inserido layout quizzquestions

--- JS/page1.js
+++ JS/page1.js
@@ -1,6 +1,6 @@
 let c = console.log.bind(document);
 
-if (localStorage.length === 0){
+if (localStorage.length === 0) {
     localStorage.setItem("idQuizz", "[1719, 1718, 1700, 1699, 1680]")
 }
 
@@ -10,21 +10,21 @@ c(idList)
 
 const userQuizzes = document.querySelector(".userQuizz .quizzes")
 const otherQuizzes = document.querySelector(".otherQuizz .quizzes")
-function gerarQuizzes(response){
-    for (let i = 0; i < response.data.length; i++){
-        for (let j = 0; j < idList.length; j++){
-            if (response.data[i].id === JSON.parse(idList)[j]){
-                userQuizzes.innerHTML +=`<div class="quizz" onclick="goToAnswerQuizz(this)"><img src="${response.data[i].image}" alt="Não foi possível carregar esta imagem."><p>${response.data[i].title}</p><div class="degrade"></div></div>`
+function gerarQuizzes(response) {
+    for (let i = 0; i < response.data.length; i++) {
+        for (let j = 0; j < idList.length; j++) {
+            if (response.data[i].id === JSON.parse(idList)[j]) {
+                userQuizzes.innerHTML += `<div id = "${i}" class="quizz" onclick="goToAnswerQuizz(this)"><img src="${response.data[i].image}" alt="Não foi possível carregar esta imagem."><p>${response.data[i].title}</p><div class="degrade"></div></div>`
             }
         }
-        otherQuizzes.innerHTML += `<div class="quizz" onclick="goToAnswerQuizz(this)"><img src="${response.data[i].image}" alt="Não foi possível carregar esta imagem."><p>${response.data[i].title}</p><div class="degrade"></div></div>`
+        otherQuizzes.innerHTML += `<div id = "${i}" class="quizz" onclick="goToAnswerQuizz(this)"><img src="${response.data[i].image}" alt="Não foi possível carregar esta imagem."><p>${response.data[i].title}</p><div class="degrade"></div></div>`
     }
 }
 
 let promiseQuizzes = axios.get("https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes")
 promiseQuizzes.then(gerarQuizzes)
 
-function goToAnswerQuizz(element){
+function goToAnswerQuizz(element) {
     document.querySelector(".quizzPlay").innerHTML = `<div><img src="${element}"> alt="Não foi possível carragar a imagem."</div>`
     document.querySelector(".home").classList.add("hide")
     document.querySelector(".quizzPlay").classList.remove("hide")
