a9060187e720b038772574c20307960d47f7b15c
Audrey-Costa <audreycosta@nano.ufrj.br>
2022-04-26 17:54:16 -0300

    Respostas escolhidas

--- JS/page1.js
+++ JS/page1.js
@@ -15,7 +15,6 @@ const otherQuizzes = document.querySelector(".otherQuizz .quizzes")
 
 //Separa os quizzes do usuário dos demais quizzes.
 function gerarQuizzes(response) {
-    c(response.data)
     for (let i = 0; i < response.data.length; i++) {
         for (let j = 0; j < idList.length; j++) {
             if (response.data[i].id === JSON.parse(idList)[j]) {
@@ -33,7 +32,6 @@ promiseQuizzes.then(gerarQuizzes)
 //Ao clicar num quizz a função é chamada fazendo a requisição do quizz em específico.
 function goToAnswerQuizz(element) {
     let id = Number(element.id)
-    c(id)
     let promiseQuizz = axios.get(`https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes/${id}`)
     promiseQuizz.then(gerarQuizz)
 }
@@ -43,7 +41,6 @@ function gerarQuizz(response) {
     const questions = response.data.questions;
 
 
-    c(response.data)
     document.querySelector(".quizzPlay").innerHTML = `<div class="headerQuizzPlay"><div class="degrade"></div><img src="${response.data.image}"alt="Não foi possível carregar a imagem, use uma url de imagem."><p>${response.data.title}</p></div>`
     //Coloca as perguntas na página.
     for (let i = 0; i < questions.length; i++) {
@@ -55,16 +52,14 @@ function gerarQuizz(response) {
                 <span class="rowOfAnswers">
              </span>
         </div>`
-        c(questions[i].answers)
         //coloca as respostas nas perguntas
         for (let a = 0; a < questions[i].answers.length; a++) {
 
             let arrRowOfAnswers = document.querySelectorAll(".rowOfAnswers"); //cria uma array de rowOfAnswers
             //entra na primeira pergunta e coloca a quantidade de answers
-            c(arrRowOfAnswers)
-            c("valor de a: " + a)
-            arrRowOfAnswers[i].innerHTML += `<div class="answer ${questions[i].answers[a].isCorrectAnswer}" onclick="answerChoose(this)">
-        <img src="${questions[i].answers[a].image}" alt="Não foi possível carregar a imagem.">
+
+            arrRowOfAnswers[i].innerHTML += `<div class="answer ${questions[i].answers[a].isCorrectAnswer}">
+        <img src="${questions[i].answers[a].image}" alt="Não foi possível carregar a imagem." onclick="answerChoose(this)">
         <div class="nonChoiced hide"></div>
         <p>${questions[i].answers[a].text}</p>
     </div>`
--- JS/page2.js
+++ JS/page2.js
@@ -1,21 +1,27 @@
 function answerChoose(element) {
     //Cria uma array com os elementos das 4 opções de resposta.
-    let row = element.parentNode.querySelectorAll(".nonChoiced")
+    let row = element.parentNode.parentNode.querySelectorAll(".nonChoiced")
 
     //Variável da div de clareamento.
-    let div = element.querySelector(".nonChoiced")
+    let div = element.parentNode.querySelector(".nonChoiced")
     div.classList.add("choice")
     //Variável do texto.
     let p = div.nextElementSibling
     row.forEach(function (element) {
+        c(element)
+        if (element.parentNode.classList.contains("true")){
+            element.parentNode.querySelector("p").classList.add("green")
+        }else{
+            element.parentNode.querySelector("p").classList.add("red")
+        }
         element.classList.remove("hide");
         //Se a resposta escolhida for a resposta certa fica verde, se não vermelho. (Não tenho certeza se tá funcionando).
-        if (element.parentNode.classList.contains("true")) {
-            p.classList.add("green")
-        } else {
-            p.classList.add("red")
-        }
+        //element.nextElementSibling.classList.add("red")
+        c(element.parentNode.classList)
     })
+    if (element.parentNode.classList.contains("true")) {
+        p.classList.add("green")
+    }
 
 
 
